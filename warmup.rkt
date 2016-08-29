7305cd2862e002c16c3f21ed74077d7a7153ac71
Argul Worm <334145486@qq.com>
2016-08-30 07:48:45 +0800

    progress

--- warmup.rkt
+++ warmup.rkt
@@ -4,4 +4,30 @@
   (define (add x y)
     (+ x y))
   (add 1 2))
-(test_function_defination)
+;(test_function_defination)
+
+(define (test_sum)
+  
+  (define (sum term a next b)
+    (define (sum_recursive term a next b)
+      (if (> a b)
+          0
+          (+ (term a) (sum_recursive term (next a) next b))))
+    ;(sum_recursive term a next b)
+    
+    (define (sum_iter term a next b result)
+      (if (> a b)
+          result
+          (sum_iter term (next a) next b (+ result (term a)))))
+    (sum_iter (lambda (x) (* x x)) a (lambda (x) (+ x 1)) b 0))
+  (sum (lambda (x) (* x x)) 1 (lambda (x) (+ x 1)) 10))
+;(test_sum)
+
+(define (div_n n x y)
+  (define (inner loop)
+    (if (> loop n)
+        (/ x y)
+        (/ x (+ y (inner (+ loop 1))))))
+  (inner 1))
+(/ 1.0 (div_n 20 1.0 1.0))
+    
